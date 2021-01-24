a3e72916fb76a4b0bdc075ed9c5ef90cd0f2716e
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-01-24 15:11:46 +0000

    commit on 'main'

--- back-end/ethnicity_df.py
+++ back-end/ethnicity_df.py
@@ -16,5 +16,9 @@ ethnicities = ethnicity_data.values()
 df = pd.DataFrame.from_dict(ethnicity_data)
 print(df)
 
+total_deaths = sum([61,19,159,85,95,67,16,8,180,80,29,30,186,85,119,75,1939,1184,112,54,188,96,514,306,78,55,525,357,144,99,351,226,286,156,20531,18201])
+print(total_deaths)
+
+
 
 
